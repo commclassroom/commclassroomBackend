67877e9c93974b05238be0b61e84be5c2ea155fb
Divyanshu Parihar <divyanshu1447@gmail.com>
2021-09-08 16:59:22 +0530

    added jest and sample test files

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ const config = require('config');
 const { app } = require('./app');
 const { logger } = require('./services/logger');
 
-const PORT = config.get('app.port');
+// const PORT = config.get('app.port');
 
-app.listen(PORT, () => {
-  logger.info(`Server listening on http://localhost:${PORT}`);
+app.listen(3000, () => {
+  logger.info(`Server listening on http://localhost:${3000}`);
 });
