63db57d52a182e3073cd05a1c97adb2b6eb8f8ed
Divyanshu Parihar <divyanshu1447@gmail.com>
2021-09-10 17:55:58 +0530

    relocated the config folder

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ const { app } = require('./app');
 const { logger } = require('./services/logger');
 
 const PORT = config.get('app.port');
-
 app.listen(PORT, () => {
   logger.info(`Server listening on http://localhost:${PORT}`);
 });
