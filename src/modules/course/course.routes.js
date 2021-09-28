1e0a2dcd98cd4f93ba4f50cd71a77d67526dee6a
tejaswini22199 <tejaswini22199@gmail.com>
2021-09-28 22:12:14 +0530

    solves issue #31 This commit adds an api( feature) to search for a course based on given title , category and number of enrolled students. When user passes a title it searches if this title is included in the course or not and displays all the courses containing the given title. Because, Ideally we want to display all the courses matching with searched title making sure user experience  and also when we pass null as title from frontend it searches based on category. wnd ignores title When we pass title , category and number of students it filters based on all three parameters (courses having title as given title , category as given category and no of enrolled students  as the  number given ) Tested using postman with edge cases and the code is working fine.

--- src/modules/course/course.routes.js
+++ src/modules/course/course.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const { logger } = require('../../services/logger');
 const router = express.Router();
 
 /** load the service */
@@ -11,11 +11,11 @@ router.get('/', async (req, res) => {
   return res.json(courseList);
 });
 
-/**to get a course by id */
-router.get('/:id', async (req, res) => {
-  const course = await CourseController.getCourseById(req.params.id);
-  return res.json(course);
-});
+// /**to get a course by id */
+// router.get('/:id', async (req, res) => {
+//   const course = await CourseController.getCourseById(req.params.id);
+//   return res.json(course);
+// });
 
 /**to add a new course */
 router.post('/new', async (req, res) => {
@@ -33,6 +33,15 @@ router.put('/update/:id', async (req, res) => {
 router.delete('/delete/:id', async (req, res) => {
   const course = await CourseController.deleteCourse(req.params.id);
   return res.json(course);
+});
+/** search for a course based on Title */
+
+router.get('/search/:title/:category/:countofstudents', async (req,res) => {
+  const title = req.params.title;
+  const category = req.params.category;
+  const countofstudents = req.params.countofstudents;
+  const filteredCourses= await CourseController.searchCourse(title , category , parseInt(countofstudents));
+  return res.json(filteredCourses);
 });
 
 /** export the routes to be binded to application */
