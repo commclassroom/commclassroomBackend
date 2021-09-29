41db9ce12949dd64dc7628111803a4b277624234
Divyanshu Parihar <divyanshu1447@gmail.com>
2021-09-29 08:55:18 +0530

    fixed method calls

--- src/modules/review/review.routes.js
+++ src/modules/review/review.routes.js
@@ -15,21 +15,21 @@ router.post('/', async (req, res) => {
 /** to update review details */
 router.patch('/', async (req, res) => {
   // req.body contains the Review object with updated details from client
-  const updatedReview = await ReviewController.updateReview(req.body);
+  const updatedReview = await ReviewController.updateReviewDetails(req.body);
   return res.json(updatedReview);
 });
 
 /** to update review details */
 router.delete('/:id', async (req, res) => {
   // req.body contains the review object id to be deleted from client
-  const removedReview = await ReviewController.removeReview(req.body.id);
+  const removedReview = await ReviewController.removeReview(req.params.id);
   return res.json(removedReview);
 });
 
 /** to update review details */
 router.get('/:userid', async (req, res) => {
   // req.body contains the User id from client
-  const userReviews = await ReviewController.getReviewsByUserId(req.body.userid);
+  const userReviews = await ReviewController.userReviews(req.params.userid);
   return res.json(userReviews);
 });
 
